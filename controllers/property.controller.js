45a032d6d957d3370636c9d1212b22433de0b4e9
Rubem Puttini <rubem.staciariniputtini@gmail.com>
2023-04-11 17:49:57 +0100

    finish the first draft

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -14,15 +14,53 @@ cloudinary.config({
 });
 
 const getAllProperties = async (req, res) => {
+  const {
+    _end,
+    _order,
+    _start,
+    _sort,
+    title_like = "",
+    propertyType = "",
+  } = req.query;
+
+  const query = {};
+
+  if (propertyType !== "") {
+    query.propertyType = propertyType;
+  }
+
+  if (title_like) {
+    query.title = { $regex: title_like, $options: "i" };
+  }
+
   try {
-    const properties = await Property.find({}).limit(req.query._end);
+    const count = await Property.countDocuments({ query });
+
+    const properties = await Property.find(query)
+      .limit(_end)
+      .skip(_start)
+      .sort({ [_sort]: _order });
+
+    res.header("x-total-count", count);
+    res.header("Access-Control-Expose-Headers", "x-total-count");
 
     res.status(200).json(properties);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
-const getPropertyDetail = async (req, res) => {};
+const getPropertyDetail = async (req, res) => {
+  const { id } = req.params;
+  const propertyExists = await Property.findOne({ _id: id }).populate(
+    "creator"
+  );
+
+  if (propertyExists) {
+    res.status(200).json(propertyExists);
+  } else {
+    res.status(404).json({ message: "Property not found" });
+  }
+};
 
 const createProperty = async (req, res) => {
   try {
@@ -59,8 +97,55 @@ const createProperty = async (req, res) => {
   }
 };
 
-const updateProperty = async (req, res) => {};
-const deleteProperty = async (req, res) => {};
+const updateProperty = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, propertyType, location, price, photo } =
+      req.body;
+
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    await Property.findByIdAndUpdate(
+      { _id: id },
+      {
+        title,
+        description,
+        propertyType,
+        location,
+        price,
+        photo: photoUrl.url || photo,
+      }
+    );
+    res.status(200).json({ message: "Property updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteProperty = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const propertyToDelete = await Property.findById({ _id: id }).populate(
+      "creator"
+    );
+
+    if (!propertyToDelete) throw new Error("Property not found");
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    propertyToDelete.remove({ session });
+    propertyToDelete.creator.allProperties.pull(propertyToDelete);
+
+    await propertyToDelete.creator.save({ session });
+    await session.commitTransaction();
+
+    res.status(200).json({ message: "Property deleted sucessfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export {
   getAllProperties,
